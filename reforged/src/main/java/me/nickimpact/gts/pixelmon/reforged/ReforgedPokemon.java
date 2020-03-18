9bb2663af533a8022ebabd7a80107ace639e73fb
Nick DeGruccio <nick.degruccio@gmail.com>
2020-03-17 20:55:16 -0700

    All recent updates

---
+++ reforged/src/main/java/me/nickimpact/gts/pixelmon/reforged/ReforgedPokemon.java
@@ -0,0 +1,101 @@
+package me.nickimpact.gts.pixelmon.reforged;
+
+import com.nickimpact.impactor.api.json.Adapter;
+import com.nickimpact.impactor.api.json.Registry;
+import com.nickimpact.impactor.api.plugin.ImpactorPlugin;
+import com.pixelmonmod.pixelmon.Pixelmon;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.entities.pixelmon.EnumSpecialTexture;
+import com.pixelmonmod.pixelmon.entities.pixelmon.stats.BonusStats;
+import com.pixelmonmod.pixelmon.entities.pixelmon.stats.ExtraStats;
+import com.pixelmonmod.pixelmon.entities.pixelmon.stats.Gender;
+import com.pixelmonmod.pixelmon.entities.pixelmon.stats.Pokerus;
+import com.pixelmonmod.pixelmon.enums.EnumGrowth;
+import com.pixelmonmod.pixelmon.enums.EnumNature;
+import com.pixelmonmod.pixelmon.enums.EnumSpecies;
+import com.pixelmonmod.pixelmon.enums.items.EnumPokeballs;
+import lombok.Getter;
+import me.nickimpact.gts.api.GtsService;
+import me.nickimpact.gts.common.plugin.GTSPlugin;
+import me.nickimpact.gts.pixelmon.GTSPokemon;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+public class ReforgedPokemon implements GTSPokemon<Pokemon, EnumSpecies, EnumNature, EnumGrowth, Gender, EnumPokeballs, EnumSpecialTexture> {
+
+	public static final ExtraStatAdapter ADAPTER = new ExtraStatAdapter(GtsService.getInstance().getRegistry().get(GTSPlugin.class));
+
+	private UUID id;
+	private EnumSpecies species;
+	private int level;
+	private int form;
+	private boolean shiny;
+	private String nickname;
+	private String ability;
+	private int abilitySlot;
+	private EnumPokeballs pokeball;
+	private EnumNature nature;
+	private EnumGrowth growth;
+	private Gender gender;
+	private StatWrapper stats;
+	private StatWrapper EVs;
+	private StatWrapper IVs;
+	private short status;
+	private EnumSpecialTexture specialTexture;
+	private String customTexture;
+	private int health;
+	private List<Integer> relearnableMoves;
+	private boolean doesLevel;
+	private EggData eggData;
+	private ItemStack heldItem;
+	private UUID originalTrainerID;
+	private String originalTrainerName;
+	private AttackWrapper[] moveset;
+
+	/** Reforged Specific Fields */
+	private List<String> specFlags;
+	private Pokerus pokerus;
+	private BonusStats bonus;
+	private ExtraStats extras;
+
+	private ReforgedPokemon(Pokemon pokemon) {
+
+	}
+
+	public static ReforgedPokemon from(Pokemon pokemon) {
+		return new ReforgedPokemon(pokemon);
+	}
+
+	@Override
+	public Pokemon construct() {
+		Pokemon pokemon = Pixelmon.pokemonFactory.create(this.id);
+		pokemon.setSpecies(this.species);
+
+
+		return pokemon;
+	}
+
+	@Override
+	public boolean doesLevel() {
+		return this.doesLevel;
+	}
+
+	private static class ExtraStatAdapter extends Adapter<ExtraStats> {
+
+		private Registry<ExtraStats> registry;
+
+		ExtraStatAdapter(ImpactorPlugin plugin) {
+			super(plugin);
+			this.registry = new Registry<>(plugin);
+		}
+
+		@Override
+		protected Registry<ExtraStats> getRegistry() {
+			return this.registry;
+		}
+	}
+
+}
--- src/main/java/me/nickimpact/gts/pixelmon/GTSPokemon.java
+++ src/main/java/me/nickimpact/gts/pixelmon/GTSPokemon.java
@@ -7,6 +7,23 @@ import net.minecraft.item.ItemStack;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * This wrapper is meant to handle the storage of a pokemon via any means. With Reforged and Generations, serializing
+ * a pokemon is nearly impossible due to the nature of the class structure (aka recursive or unserializable data structures).
+ *
+ * <p>With this class, we simply deserialize the pokemon ourselves, providing types that will permit the storage
+ * of pokemon safely via desirable means. So if a user were to use GSON, you can successfully serialize a pokemon
+ * converted into this type.</p>
+ *
+ * @param <Pokemon> The actual implementation of a pokemon. This is what either reforged or generations uses to handle the
+ *                 data storage of the pokemon
+ * @param <Species> The species of the pokemon
+ * @param <Nature> The nature of the pokemon
+ * @param <Growth> The growth of the pokemon
+ * @param <Gender> The gender of the pokemon
+ * @param <Pokeball> The pokeball the pokemon was captured in / inherited
+ * @param <SpecialTexture> The special texture set on this pokemon
+ */
 public interface GTSPokemon<Pokemon, Species, Nature, Growth, Gender, Pokeball, SpecialTexture> {
 
 	Pokemon construct();
@@ -79,4 +96,6 @@ public interface GTSPokemon<Pokemon, Species, Nature, Growth, Gender, Pokeball, 
 		private int ppLevel;
 
 	}
+
+
 }
