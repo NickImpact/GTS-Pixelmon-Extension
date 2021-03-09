03a55bee1a413cd6466397d67d10e07c31eebe22
Nick DeGruccio <nick.degruccio@gmail.com>
2021-03-08 22:07:48 -0700

    Fix legendary minimum price keys

--- reforged/sponge/src/main/java/net/impactdev/gts/reforged/sponge/config/ReforgedConfigKeys.java
+++ reforged/sponge/src/main/java/net/impactdev/gts/reforged/sponge/config/ReforgedConfigKeys.java
@@ -43,8 +43,8 @@ public class ReforgedConfigKeys implements ConfigKeyHolder {
     public static final ConfigKey<Boolean> MIN_PRICING_HA_ENABLED = booleanKey("price-controls.minimum.ha.enabled", true);
     public static final ConfigKey<Double> MIN_PRICING_HA_PRICE = doubleKey("price-controls.minimum.ha.price", 5000);
 
-    public static final ConfigKey<Boolean> MIN_PRICING_LEGEND_ENABLED = booleanKey("price-controls.minimum.legend.enabled", true);
-    public static final ConfigKey<Double> MIN_PRICING_LEGEND_PRICE = doubleKey("price-controls.minimum.legend.price", 5000);
+    public static final ConfigKey<Boolean> MIN_PRICING_LEGEND_ENABLED = booleanKey("price-controls.minimum.legendary.enabled", true);
+    public static final ConfigKey<Double> MIN_PRICING_LEGEND_PRICE = doubleKey("price-controls.minimum.legendary.price", 5000);
 
     public static final ConfigKey<Boolean> MIN_PRICING_SHINY_ENABLED = booleanKey("price-controls.minimum.shiny.enabled", true);
     public static final ConfigKey<Double> MIN_PRICING_SHINY_PRICE = doubleKey("price-controls.minimum.shiny.price", 5000);
